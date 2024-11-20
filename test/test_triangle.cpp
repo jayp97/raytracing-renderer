ecfa641ee44e8769c5daff8cb68fcab783cba557
Jay Pinion <jaypinion97@gmail.com>
2024-11-20 18:04:06 +0000

    fixed test_triangle

--- test/test_triangle.cpp
+++ test/test_triangle.cpp
@@ -1,28 +1,30 @@
-// test_triangle.cpp
 #include "catch_amalgamated.hpp"
 #include "../include/Triangle.h"
 #include "../include/Ray.h"
 #include "../include/Vector3.h"
+#include "../include/Intersection.h"
 
 const float tolerance = 1e-5;
 
 TEST_CASE("Triangle Intersection - Ray Hits Triangle", "[TRIANGLE]")
 {
     Material defaultMaterial;
+    // Define a triangle in the X-Y plane
     Triangle triangle(Vector3(0, 0, 0), Vector3(1, 0, 0), Vector3(0, 1, 0), defaultMaterial);
-    Ray ray(Vector3(0.25f, 0.25f, -1), Vector3(0, 0, 1));
+    Ray ray(Vector3(0.25f, 0.25f, -1), Vector3(0, 0, 1)); // Ray pointing towards the triangle
 
-    float t, u, v;
-    REQUIRE(triangle.intersect(ray, t, u, v) == true);
-    REQUIRE(t > 0);
+    Intersection hit;
+    REQUIRE(triangle.intersect(ray, hit) == true);
+    REQUIRE(hit.distance > 0); // Ensure the intersection distance is valid
 }
 
 TEST_CASE("Triangle Intersection - Ray Misses Triangle", "[TRIANGLE]")
 {
     Material defaultMaterial;
+    // Define a triangle in the X-Y plane
     Triangle triangle(Vector3(0, 0, 0), Vector3(1, 0, 0), Vector3(0, 1, 0), defaultMaterial);
-    Ray ray(Vector3(-1, -1, -1), Vector3(0, 0, 1));
+    Ray ray(Vector3(-1, -1, -1), Vector3(0, 0, 1)); // Ray pointing away from the triangle
 
-    float t, u, v;
-    REQUIRE(triangle.intersect(ray, t, u, v) == false);
+    Intersection hit;
+    REQUIRE(triangle.intersect(ray, hit) == false); // Expect no intersection
 }
